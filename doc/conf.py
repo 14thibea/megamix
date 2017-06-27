b2e9b04720ed0a93ec53e2fb539a5db8ca0432f0
14thibea <elina.ts@free.fr>
2017-06-27 11:32:35 +0200

    added viewcode

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon',
-    'sphinx.ext.ifconfig']
+    'sphinx.ext.viewcode','sphinx.ext.ifconfig']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
